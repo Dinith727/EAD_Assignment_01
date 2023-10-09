d25f09b280f1abb5b3aca10159d3915cff63c790
OshidhieP <OshidhieP@lolctech.com>
2023-10-09 14:11:49 +0530

    Services Added

---
+++ web/Services/MongoDBService.cs
@@ -0,0 +1,48 @@
+using System.Threading.Tasks;
+using web_service.Models;
+using web_service.Helper;
+using web_service.Operation;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace web_service.Services
+{
+    public class MongoDBService
+    {
+        private readonly IMongoCollection<AdminModel> _adminCollection;
+        private readonly IMongoCollection<TravellerModel> _travellerCollection;
+        private readonly IMongoCollection<TrainScheduleModel> _trainScheduleCollection;
+        private readonly IMongoCollection<ReservationModel> _reservationCollection;
+        public readonly IMongoCollection<BsonDocument> _genericBsonDoc;
+
+        public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings)
+        {
+            MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+            IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+
+            _adminCollection = database.GetCollection<AdminModel>("admin");
+            _travellerCollection = database.GetCollection<TravellerModel>("traveller");
+            _trainScheduleCollection = database.GetCollection<TrainScheduleModel>("trainShedule");
+            _reservationCollection = database.GetCollection<ReservationModel>("reservation");
+            _genericBsonDoc = database.GetCollection<BsonDocument>("reservation");
+
+            var usernameIndex = new CreateIndexModel<AdminModel>(
+                Builders<AdminModel>.IndexKeys.Ascending(model => model.username),
+                new CreateIndexOptions { Unique = true }
+            );
+            _adminCollection.Indexes.CreateOneAsync(usernameIndex);
+
+        }
+
+        // Admin Services
+        public AdminOperations Admin => new AdminOperations(_adminCollection);
+
+        // Traveller Services
+        public TravellerOperation Traveller => new TravellerOperation(_travellerCollection);
+        public TrainScheduleOperation Train => new TrainScheduleOperation(_trainScheduleCollection);
+        public ReservationOperation Reservation => new ReservationOperation(_reservationCollection, _genericBsonDoc);
+
+
+    }
+}
