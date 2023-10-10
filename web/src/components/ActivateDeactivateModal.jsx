75524f140bfcc97c8ca37af2ec9c5b277902cdcd
Dinithma <tharusha.dinithma@gmail.com>
2023-10-10 12:55:17 +0530

    ActivateDeactivate Model Implemented

---
+++ web/src/components/ActivateDeactivateModal.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+
+const ActivateDeactivateModal = ({ show, onHide, onConfirm, action }) => {
+	return (
+		<Modal show={show} onHide={onHide}>
+			<Modal.Header closeButton>
+				<Modal.Title>Confirm {action}</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				Are you sure you want to {action.toLowerCase()} this user?
+			</Modal.Body>
+			<Modal.Footer>
+				<Button variant="secondary" onClick={onHide}>
+					Cancel
+				</Button>
+				<Button variant="primary" onClick={onConfirm}>
+					{action}
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	);
+};
+
+export default ActivateDeactivateModal;
