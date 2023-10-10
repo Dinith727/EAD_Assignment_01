3cc21dcabdb90e8322879cf2adea55bce7813e13
Dinithma <tharusha.dinithma@gmail.com>
2023-10-10 12:57:23 +0530

    Train List Component Implemented

---
+++ web/src/components/TrainList.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import TrainDetail from "./TrainDetail";
+import { getAllTrains } from "../utils/api/trainShedule";
+
+const TrainList = () => {
+	const [trains, setTrains] = useState([
+		{
+			_id: "",
+			trainName: "",
+			from: "",
+			to: "",
+			startTime: "",
+			arrivalTime: "",
+			price: {
+				bussiness: "",
+				economic: "",
+			},
+			active: false,
+			published: false,
+			_isDeleted: false,
+		},
+	]);
+
+	useEffect(() => {
+		async function getTrains() {
+			const _res = await getAllTrains();
+			setTrains(_res.data);
+		}
+		getTrains();
+	}, []);
+
+	return (
+		<div className="train-list">
+			<table className="table">
+				<thead>
+					<tr>
+						<th>Train Name</th>
+						<th>From</th>
+						<th>To</th>
+
+						<th>Arrival Time</th>
+						<th>Departure Time</th>
+						<th>Edit</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					{trains.map((train, index) => (
+						<TrainDetail key={index} train={train} />
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default TrainList;
