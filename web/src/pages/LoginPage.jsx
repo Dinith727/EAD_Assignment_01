bc4906050de0466b1880d65691ae0122b645fc1d
Uvindushamodya <Uvinduabeykoon123@gmail.com>
2023-10-10 21:12:43 +0530

    Login page added

---
+++ web/src/pages/LoginPage.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import LoginComp from "../components/LoginComp";
+
+function LoginPage() {
+	const backgroundImageUrl =
+		"https://images.pexels.com/photos/716834/pexels-photo-716834.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
+	const containerStyle = {
+		backgroundImage: `url(${backgroundImageUrl})`,
+		backgroundSize: "cover",
+		backgroundPosition: "center",
+		minHeight: "100vh",
+		paddingTop: "80px",
+	};
+
+	const headingStyle = {
+		textAlign: "center",
+		color: "white",
+	};
+
+	return (
+		<div style={containerStyle}>
+			<h1 style={headingStyle}>Booking Management System</h1>
+			<LoginComp topic="Admin" />
+		</div>
+	);
+}
+
+export default LoginPage;
