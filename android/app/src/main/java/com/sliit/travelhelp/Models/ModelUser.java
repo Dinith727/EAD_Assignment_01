f25f35e29ffc86515840d69af8f1e321bc0043e3
Uvindushamodya <Uvinduabeykoon123@gmail.com>
2023-10-10 13:19:05 +0530

    Added user register and login model files

---
+++ android/app/src/main/java/com/sliit/travelhelp/Models/ModelUser.java
@@ -0,0 +1,47 @@
+package com.sliit.travelhelp.Models;
+
+public class ModelUser {
+    private String id;
+    private String email;
+    private String NIC;
+    private String fullName;
+
+    public ModelUser(String id, String email, String NIC, String fullName) {
+        this.id = id;
+        this.email = email;
+        this.NIC = NIC;
+        this.fullName = fullName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
